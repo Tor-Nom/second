9dcf25a9b70e526836b0d1a5ec2f193936c39f73
liuchao02 <qq15810595175>
2019-10-24 12:36:28 +0800

    mertrics debug

--- example/test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/Tor-Nom/second"
-)
-
-func main() {
-	second.Metrics(policyGetUserInsureListService, "error", "GetPlicyInfo.res.gpi", second.LEVEL_ERROR, "GetPlicyInfo.res.gpi err")
-}
